7456db6b7639659a37bcbb053950135dd1f036b2
Shiva Surya <shivasurya470@gmail.com>
2026-01-15 21:59:50 +0530

    optimized solution for sorting 0s, 1s and 2s

--- c++/sortArray0_1_2.cpp
+++ c++/sortArray0_1_2.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    vector<int> nums = {0,0,0,0,1,1,1,2,2};
-    int count0 = 0, count1 = 0, count2 = 0;
+    vector<int> nums = {0,0,2,1,2,1,0,1};  
     int n = nums.size();
-    for(int i = 0; i < n; i++){
-        if(nums[i] == 0) count0++;
-        else if(nums[i] == 1) count1++;
-        else count2++;
+    int low = 0, mid = 0, high = n-1;
+    while(mid <= high){
+        if(nums[mid] == 0){
+            swap(nums[mid] , nums[low]);
+            low++;
+            mid++;
+        } 
+        else if(nums[mid] == 1){
+            mid++;
+        } 
+        else{
+            swap(nums[mid] , nums[high]);
+                high--;
+             }
     }
-
-    int idx = 0;
-    for(int i = 0; i < count0; i++){
-        nums[idx] = 0;
-        idx++;
-    } 
-    for(int i = 0; i < count1; i++){
-        nums[idx] = 1;
-        idx++;
-    }
-    for(int i = 0; i < count2; i++){
-        nums[idx] = 2;
-        idx++;
-    }
-
     for(int i = 0; i < n; i++){
         cout << nums[i] << " ";
     }
-
 }
